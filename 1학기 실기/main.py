4bd076a6ccbb9061d1505f1791925961fd9056c9
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-06-09 22:35:17 +0900

    기말과제

--- 1학기 실기/main.py
+++ 1학기 실기/main.py
@@ -76,7 +76,6 @@ def write_stugrade():
             if dsubject in subject:
                 dfsubject = get_subject(dsubject) #과목 정보
                 ddsubject, dcredit, dcode, dpname = dfsubject #과목명, 학점, 학과코드, 교수아이디
-                # print("과목명 : %s   과목코드 : %s    학점 : %s    점수 : %s" %(ddsubject, dsubject, dcredit,dscore))
                 ddscore = rating(dscore)
                 print("%s\t   %s\t   %s\t   %s\t   %s" % (ddsubject, dsubject, dcredit, dscore, ddscore))
 
@@ -146,8 +145,8 @@ def selmenu02_write():
 def stats_subject():
     count = 0  #수강생 수
     avscore = 0 #평균 점수
-    maxscore = 0 #최고 점수
-    minscore = 0 #최저 점수
+    maxscore = [] #최고 점수
+    minscore = [] #최저 점수
 
     #타이틀 (학과명)
     insubject = input("> 학과코드 : ") #학과코드
@@ -157,16 +156,59 @@ def stats_subject():
     print("학과명 : %s" % (sname))
 
     #리스트 (과목명, 과목코드, 학점, 수강생 수, 평균 점수, 최고점수, 최저점수)
-    print("   과목명       과목코드       학점      수강생 수      평균 점수      최고점수      최저점수")
+    print("   과목명       과목코드     학점     수강생 수     평균 점수     최고점수     최저점수")
     for scode, val in subject.items():
         dsubject, dcredit, dcode, dpname = val #과목명, 학점, 개설학과코드, 담당교수 아이디
         if insubject in dcode: #과목코드 찾기 (키)
-            print("%s\t   %s\t   %s\t   " % (dsubject, scode, dcredit))
             #수강생 수, 평균 점수, 최고, 최저 점수 구하기
             for dclassof, dsubjnum, dscore in stscore:  # 학번, 과목코드, 점수
                 if scode in dsubjnum:
-                    count += 1
+                    dscore = int(dscore) #점수 int로 변환
+                    maxscore.append(dscore) #최고점수 리스트로 모으기
+                    minscore.append(dscore) #최저점수 리스트로 모으기
+                    dscore = int(dscore) #점수 int로 변경
+                    avscore = avscore + dscore #점수 합계
+                    count += 1 #학생 수
+            aavscore = avscore / count
+            print("%s\t    %s\t    %s\t     %2d\t         %d\t         %d\t        %d" %(dsubject, scode, dcredit, count, aavscore, max(maxscore), min(minscore)))
+        count = 0 #학생수 초기화
+        avscore = 0 #점수 합계 초기화
+        maxscore = [] #최고점수 리스트 초기화
+        minscore = [] #최저점수 리스트 초기화
 
+def stats_professor():
+    count = 0  # 수강생 수
+    sumsubject = 0 # 과목 수
+    sumcredit = 0 # 학점 수
+    sumstudent = 0 # 수강생 합계
+
+    # 타이틀 (학과명)
+    insubject = input("> 학과코드 : ")  # 학과코드
+    sname = get_dept(insubject)
+
+    print("_" * 50)
+    print("학과명 : %s" % (sname))
+    print()
+
+    # 리스트(교수아이디, 교수명, 과목명, 학점, 수강생 수)
+    print("교수아이디    교수명        과목명        학점      수강생 수")
+    for scode, val in subject.items(): #과목 딕셔너리 key, val 값 분리
+        dsubject, dcredit, dcode, dproid = val #과목명, 학점, 개설학과코드, 담당교수 아이디
+        dprof = get_prof(dproid)
+        pname, page = dprof #교수명, 나이
+        sumsubject += 1 #과목 수 카운트
+        dcredit = int(dcredit) #학점 int로 변환
+        sumcredit = sumcredit + dcredit #총 학점 카운트
+        for dclassof, dsubjnum, dscore in stscore:  # 학번, 과목코드, 점수
+            if scode in dsubjnum:
+                count += 1  # 학생 수
+        print("%6s\t    %3s\t   %6s\t   %2s\t       %d" %(dproid, pname, dsubject, dcredit, count))
+        sumstudent = sumstudent + count
+        count = 0  # 학생수 초기화
+
+    #테일 (과목 수, 학점 수, 수강생 합계)
+    print()
+    print("과목 수 : %d\t  학점 수 : %d\t  수강생 합계 : %d\t" %(sumsubject,sumcredit, sumstudent))
 
 def selmenu03_stats():
     while True:
@@ -180,7 +222,111 @@ def selmenu03_stats():
             if selno == 1:
                 stats_subject()
             elif selno == 2:
-                stats_subgrade()
+                stats_professor()
+            else:
+                print("!잘못된 입력!")
+
+def score_format(): #데이터 관리 > 성적 > 수정 (4-1-2)
+    instudent, insubject, inscore = input("> 수정할 데이터(학번, 과목코드, 점수) : ").split()
+    removedata = [] #지울 데이터 리스트
+    indata = [] #추가할 데이터 리스트
+
+    removedata.append(instudent)  # 지울 데이터 리스트로 append하기
+    removedata.append(insubject)
+    removedata.append(inscore)
+
+    if removedata in stscore: #성적 리스트에 일치하는 데이터가 있는지 확인
+        print("수정할 데이터 > 학번 : %s   과목코드 : %s    점수 : %s"
+              %(instudent, insubject, inscore)) #수정할 데이터가 맞는지 확인하는 코드
+        stscore.remove(removedata)  # 성적 리스트에 데이터 지우기
+        # print(stscore) #지운 결과 값
+
+        fstudent, fsubject, fscore = input("> 수정할 내용(학번, 과목코드, 점수) : ").split() #수정할 내용 받기
+        indata.append(fstudent)  # 추가(수정)할 데이터 리스트로 받기
+        indata.append(fsubject)
+        indata.append(fscore)
+
+        if len(indata[0]) == 6 and len(indata[1]) == 5 and len(indata[2]) == 2:
+            stscore.append(indata)  # 성적 리스트에 추가하기
+            # print(stscore) #수정된 결과 값
+            print("수정이 완료되었습니다.")
+            removedata = []  # 지울 데이터 초기화
+            indata = []  # 추가할 데이터 초기화 (중복 방지)
+        else:
+            print("잘못된 내용입니다.(글자 수) ")
+            removedata = []  # 지울 데이터 초기화
+            indata = []  # 추가할 데이터 초기화 (중복 방지)
+    else:
+        removedata = [] #지울 데이터 초기화
+        print("일치하는 데이터가 없습니다.")
+
+def score_Additional(): #데이터 관리 > 성적 > 추가 (4-1-3)
+    instudent, insubject, inscore = input("> 추가할 데이터(학번, 과목코드, 점수) : ").split()
+    indata = [] #추가할 데이터 리스트
+
+    indata.append(instudent)  # 추가(수정)할 데이터 리스트로 받기
+    indata.append(insubject)
+    indata.append(inscore)
+
+    stscore.append(indata)  # 성적 리스트에 추가하기
+    # print(stscore) #추가된 결과 값
+    print("추가가 완료되었습니다.")
+
+def score_remove(): #데이터 관리 > 성적 > 삭제 (4-1-4)
+    instudent, insubject, inscore = input("> 삭제할 데이터(학번, 과목코드, 점수) : ").split()
+    inremove = [] #삭제할 데이터 리스트
+
+    inremove.append(instudent)  # 삭제할 데이터 리스트로 받기
+    inremove.append(insubject)
+    inremove.append(inscore)
+
+    if inremove in stscore:
+        stscore.remove(inremove)  # 성적 리스트에서 삭제하기
+        # print(stscore) #삭제된 결과 값
+        print("삭제가 완료되었습니다.")
+        inremove = [] #삭제 데이터 초기화 (중복 방지)
+
+    else:
+        print("일치하는 데이터가 없습니다.")
+        inremove = []  # 삭제 데이터 초기화 (중복 방지)
+
+def up_score(): #데이터 관리 > 성적
+    print("1:검색    2:수정    3:추가    4:삭제   0:복귀")
+    selno = input("<작업 선택> ")
+    if len(selno) == 0 or selno == '0':
+        return
+    if len(selno) >= 0 and type(int(selno)) is int:
+        selno = int(selno)
+        if selno == 1:
+            find_stscore()
+        elif selno == 2:
+            score_format()
+        elif selno == 3:
+            score_Additional()
+        elif selno == 4:
+            score_remove()
+        else:
+            print("!잘못된 입력!")
+
+def selmenu04_up(): #데이터 관리
+    while True:
+        print("_"*50)
+        print("1:성적   2:학생   3:교수   4:과목   5:수강   0:복귀")
+        selno = input("<작업 선택> ")
+        if len(selno) == 0 or selno == '0':
+            return
+        if len(selno) >= 0 and type(int(selno)) is int:
+            selno = int(selno)
+            if selno == 1:
+                up_score()
+            elif selno == 2:
+                up_task()
+            elif selno == 3:
+                up_task()
+            elif selno == 4:
+                up_task()
+            elif selno == 5:
+                up_task()
             else:
                 print("!잘못된 입력!")
 
@@ -194,8 +340,8 @@ def sel_task(): #작업 선택 메뉴
         return selno
 
 ########## Main ################
-while True:         #작업 선택
-    selno = sel_task()      #작업 번호 선택
+while True:    #작업 선택
+    selno = sel_task()  #작업 번호 선택
     if selno == SEARCH:
         selmenu01_find()
     elif selno == LIST:
@@ -204,7 +350,7 @@ while True:         #작업 선택
         selmenu03_stats()
     elif selno == DATAMG:
         selmenu04_up()
-    elif selno == END:          #(x)끝내기
+    elif selno == END:     #(x)끝내기
         break
     else:
         break
